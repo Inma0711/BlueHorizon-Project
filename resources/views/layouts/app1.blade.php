d52e0688d0fc5e3ba447cea277b7fc905da7f9a6
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-18 12:57:33 +0100

    feat: design changes

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="{{ asset('css/app1.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/app1.css') }}?v={{ time() }}">
         <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/0/614.png" type="image/x-icon">
         <title>@yield('title', "BlueHorizon Airline")</title>
     </head>
