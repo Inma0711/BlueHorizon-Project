58746f59972a9b46291705711a185ddbf13fb382
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-18 19:07:11 +0100

    feat: add the relations and functions

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -3,21 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Flight extends Model
 {
-   
+
     use HasFactory;
 
     protected $fillable = [
-        'plane_id', 
-        'date', 
+        'plane_id',
+        'date',
         'departure_time',
         'arrival_time',
-        'departure_location', 
-        'arrival_location', 
+        'departure_location',
+        'arrival_location',
         'available_seats',
         'status'
     ];
+    public function plane(): BelongsTo
+    {
+        return $this->belongsTo(Plane::class, 'plane_id');
+    }
+
+    public function flightBookings(): HasMany
+    {
+        return $this->hasMany(Reservation::class, 'flight_id');
+    }
+
+    // Verificar si hay asientos disponibles
+    public function hasAvailableSeats(): bool
+    {
+        return $this->available_seats > 0;
+    }
+
+    // Obtener todos los usuarios que han reservado este vuelo
+    public function passengers()
+    {
+        return $this->hasManyThrough(User::class, Reservation::class, 'flight_id', 'id', 'id', 'user_id');
+    }
 }
